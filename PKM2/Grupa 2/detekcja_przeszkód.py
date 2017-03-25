79b359a73cf3b72e620249a9caf0b9762658ce79
da_wid <da_wid@onet.pl>
2017-03-25 10:27:48 +0100

    male usprawnienia i dodanie komenatrzy

--- PKM2/Grupa 2/detekcja_przeszkód.py
+++ PKM2/Grupa 2/detekcja_przeszkód.py
@@ -5,26 +5,28 @@ video = cv2.VideoCapture('C:\Users\Dawid\PycharmProjects\PKM\przeszkody.avi')
 
 while(video.isOpened()):
 
-    # Take each frame
+    # pobieramy ramke
     _, frame = video.read()
-    subframe = frame[300:550, 150:350]
+    # wycinamy fragment, na ktorym widac tory
+    subframe = frame[300:600, 150:350]
 
-    # Convert BGR to HSV
+    # konwertujemy BGR do HSV
     hsv = cv2.cvtColor(subframe, cv2.COLOR_BGR2HSV)
 
+    # definiujemy zakres koloru brazowego (mozna tutaj jeszcze poeksperymentowac)
+    lower_brown = np.array([0, 0, 0])
+    upper_brown = np.array([30, 100, 130])
 
-    # define range of blue color in HSV
-    lower_blue = np.array([0, 0, 0])
-    upper_blue = np.array([30, 100, 130])
+    # maskowanie w celu uzyskania tylko brazowgo koloru
+    mask = cv2.inRange(hsv, lower_brown, upper_brown)
 
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
-    #upiekszenia
+    # Thresholding, ktorego celem jest zdefiniowanie obszaru widocznosci torow
     thresh = cv2.threshold(mask, 25, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations=2)
     derp, cnts, cos = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # tutaj przechowujemy obszary
+    contours = []
 
     for c in cnts:
         # ignorowanie zbyt malych obszarow
@@ -33,19 +35,22 @@ while(video.isOpened()):
            #naniesienie ramki
         #(x, y, w, h) = cv2.boundingRect(c)
         #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        widactory = True
+        else:
+            widac_tory = True
+            contours.append(cv2.contourArea(c))
 
-    if(not widactory):
-        cv2.putText(frame,'mamy przeszkode',(30,150),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255))
+    if(not widac_tory):
+        cv2.putText(frame,'PRZESZKODA!',(30,150),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,255))
+    else:
+        #fragment kodu potrzebny do pozniejszego ulepszenia algorytmu
+        max_contour = max(contours)
+        print(max_contour)
 
-    widactory = False
-    # Bitwise-AND mask and original image
-    #res = cv2.bitwise_and(frame,frame, mask= mask)
+    widac_tory = False
 
     cv2.imshow('frame',frame)
-    #cv2.imshow('mask',mask)
-    #cv2.imshow('res',res)
 
+    # warunek ktory zapewnia odstep miedzy klatkami i umozliwa wyjscia
     if (cv2.waitKey(10) & 0xFF == ord('q')):
         break
 
