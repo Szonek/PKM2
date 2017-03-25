00d9f318d4dbf2d7ab1ec51bca5e6d45a290bed1
Medioxx <marek1cz@gmail.com>
2017-03-25 22:23:16 +0100

    Zamieszczenie komentarzy

--- PKM2/Grupa 2/main_recognition_by_color.py
+++ PKM2/Grupa 2/main_recognition_by_color.py
@@ -1,4 +1,3 @@
-# import the necessary packages
 from collections import deque
 import numpy as np
 import argparse
@@ -6,46 +5,39 @@ import imutils
 import cv2
 
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize th
-# list of tracked points
-greenLower = (0, 100, 140)
-greenUpper = (40, 255, 255)
-# if a video path was not supplied, grab the reference
-# to the webcam
+# definiujemy zakresy koloru HSV
+redLower = (0, 100, 140)
+redUpper = (40, 255, 255)
 
 
-# otherwise, grab a reference to the video file
 
+
+#pobieramy nasz film
 camera = cv2.VideoCapture("czysty.avi")
 
-# keep looping
+
 while (camera.isOpened()):
-    # grab the current frame
+    #pobieramy aktualn¹ klatkê filmu do przetwarzania
     (grabbed, frame) = camera.read()
+    #zawê¿amy zakres poszukiwañ
     obraz = frame[0:800, 400:800]
 
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    #frame = imutils.resize(frame, width=600)
-    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    
+    #przechodzimy z RGB na HSV
     hsv = cv2.cvtColor(obraz, cv2.COLOR_BGR2HSV)
 
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    #nak³adamy maskê oraz poszukujemy takich zakresów kolorów w danej klatce
+    mask = cv2.inRange(hsv, redLower, redUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)[-2]
     center = None
 
-    # only proceed if at least one contour was found
+    #odfiltrowujemy ma³e i niepoprawne skupiska koloru
     if len(cnts) > 10:
+        #wypisujemy napis na danej klatce
         cv2.putText(frame, "Peron!", (50, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, .9, (255, 0, 100))
     cv2.imshow("Frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
