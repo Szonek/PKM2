a08b0d60e9ad370792dcae8ed8725e369b67876a
Adam Belniak <adamos581@wp.pl>
2017-04-12 22:57:24 +0200

    Usuniecie efektu migotania napisy75 %skutecznosc

--- PKM2/Grupa 2/main.py
+++ PKM2/Grupa 2/main.py
@@ -1,43 +1,50 @@
 import numpy as np
 import cv2
 
-#Wersja opencv 2.4.13.2
-cap = cv2.VideoCapture('przeszkody.avi')
-#inicjalizacja obiektu odpowiedzialneo za porywnywanie klatek
-fgbg = cv2.BackgroundSubtractorMOG()
+cap = cv2.VideoCapture('filmiki/przeszkody.avi')
+fgbg = cv2.createBackgroundSubtractorMOG2()
 
 ret, frame = cap.read()
 height, width = frame.shape[:2]
 prev_frame = np.zeros([130, width])
+licznik =0
 
 while(cap.isOpened()):
     ret, frame = cap.read()
+    history = 4
 
-    # w tym miejscu okreslony czestotliwosc odswiezania historii
-    history = 30
-
-    #Sprawdzamy tylko dolna czesc obrazu
     obraz = frame[350:height]
-    #Porownanie nowej klatki  z ostatnio zapisana klatka
     fgmask = fgbg.apply(obraz, learningRate=1.0/history)
     gray = cv2.cvtColor(obraz, cv2.COLOR_BGR2GRAY)
 
+
+    #dif = frame - prev_frame;
+
     prev_frame = gray
 
-    #Sprawdzamy czy sa jakies rozniece miedzy nimi 
+    #first = fgmask[0:350,0:50]
+
     uklad = np.sum(fgmask)
-    if(uklad == 0):
-     font = cv2.FONT_HERSHEY_SIMPLEX
-     cv2.putText(frame, 'stoi', (10, 500), font, 4, (255, 255, 255), 2)
-    else:
-        font = cv2.FONT_HERSHEY_SIMPLEX
+    print(licznik)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    #Dodaje opoznienie przy przelaczaniu sie stanu
+    if(uklad < 40000):
+     licznik-=1
+    elif (uklad > 40000):
+     licznik =6
+
+    if(licznik <=0):
+        cv2.putText(frame, 'stoi', (100, 100), font, 3, (255, 255, 255), 2)
+
+    if(licznik > 0):
         cv2.putText(frame, 'Jedzie', (100, 100), font, 3, (255, 255, 255), 2)
 
-    #qcv2.imshow('first', first)
-    #cv2.imshow('frame',fgmask)
+
+    #cv2.imshow('first', first)
+    cv2.imshow('frame',fgmask)
     cv2.imshow('frames', frame)
     if cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite('ramka.jpg',frame)
+        cv2.imwrite('trz_ramka.jpg',frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
