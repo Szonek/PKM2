8dbfad60863adc23b899d7dc0792adb18ae2095b
tompawlo <pawlowski.tomq@gmail.com>
2017-04-19 11:01:40 +0200

    ai-ball rec by collor
    działający program rozpoznawania peronu pracująca na streamie z kamerki

---
+++ PKM2/Grupa 1/ai-ballSTREAM_main_recognition_by_color.py
@@ -0,0 +1,78 @@
+# Python version 3.6
+
+import numpy as np
+import cv2
+import time
+import requests
+import threading
+from threading import Thread, Event, ThreadError
+from cv2 import __version__
+
+import argparse
+from collections import deque
+# definiujemy zakresy koloru HSV
+redLower = (0, 110, 0)
+redUpper = (3, 255, 255)
+
+class Cam():
+    def __init__(self, url):
+
+        self.stream = requests.get(url, stream=True)
+        self.thread_cancelled = False
+        self.thread = Thread(target=self.run)
+        print("camera initialised")
+
+    def start(self):
+        self.thread.start()
+        print("camera stream started")
+
+    def run(self):
+        bytes = b''
+        while not self.thread_cancelled:
+            try:
+                bytes += self.stream.raw.read(1024)
+                a = bytes.find(b'\xff\xd8')
+                b = bytes.find(b'\xff\xd9')
+                if a != -1 and b != -1:
+                    jpg = bytes[a:b + 2]
+                    bytes = bytes[b + 2:]
+                    frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                    #------------------ insert algorythms HERE ------------------
+                    obraz = frame[0:800, 400:800]
+                    hsv = cv2.cvtColor(obraz, cv2.COLOR_BGR2HSV)
+
+                    # nak³adamy maskê oraz poszukujemy takich zakresów kolorów w danej klatce
+                    mask = cv2.inRange(hsv, redLower, redUpper)
+                    mask = cv2.erode(mask, None, iterations=2)
+                    mask = cv2.dilate(mask, None, iterations=2)
+                    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                            cv2.CHAIN_APPROX_SIMPLE)[-2]
+                    center = None
+
+                    # odfiltrowujemy ma³e i niepoprawne skupiska koloru
+                    if len(cnts) > 3:
+                        # wypisujemy napis na danej klatce
+                        cv2.putText(frame, "Peron!", (50, 50), cv2.FONT_HERSHEY_COMPLEX_SMALL, .9, (255, 0, 100))
+                    # Display the resulting frame
+                    cv2.imshow('Video', frame)
+                    # ------------------ algorythms end HERE ------------------
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        exit(0)
+            except ThreadError:
+                self.thread_cancelled = True
+
+    def is_running(self):
+        return self.thread.isAlive()
+
+    def shut_down(self):
+        self.thread_cancelled = True
+        # block while waiting for thread to terminate
+        while self.thread.isAlive():
+            time.sleep(1)
+        return True
+
+
+print('OpenCV version',__version__)
+url = 'http://192.168.2.1/?action=stream'
+cam = Cam(url)
+cam.start()
