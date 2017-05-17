1cb23645ba8c822ff44ce885fbec984a416f1354
barorlowski <barorlowski@gmail.com>
2017-05-17 22:44:12 +0200

    GUI vol4
    Dodanie obslugi tel

--- PKM2/MAIN/GUI.py
+++ PKM2/MAIN/GUI.py
@@ -6,8 +6,14 @@ import cv2
 import numpy as np
 import os
 
-tel = {'peron':True,'zajezdnia':False,'reka':False,'tory':False,"czerwony":False,'twarz':False}
-cap = cv2.VideoCapture("zajezdnia3.avi")
+#Zdecydowac ktora opcje tel wybieramy
+tel = {'peron':False,'zajezdnia':False,'reka':False,'tory':False,"czerwony":False,'twarz':False}
+
+tel = {'peron':False,'zajezdnia':False,'reka':False,'przeszkoda':False,'czerwony':False,'twarz':False, 'ruch':False}
+#cap = cv2.VideoCapture("zajezdnia3.avi")
+
+#A to gdzie ? 
+#os.system("python skryptRozdzielajacy.py "+str(filmOrCam)+ " czysty.avi "+str(tel))
 
 class Okno(QMainWindow):
     def __init__(self):
@@ -15,55 +21,73 @@ class Okno(QMainWindow):
         self.ui = loadUi('PKM_GUI.ui',self)       
         self.ui.button_stream_start.clicked.connect(self.stream_start)
         self.ui.button_nagranie_start.clicked.connect(self.nagranie_start)
-       # self.ui.button_kalibruj.clicked.connect(self.kalibruj_start)
-        
-        #if self.ui.comboBox.currentIndex = 0:
-                
-            
-            
+        # self.ui.button_kalibruj.clicked.connect(self.kalibruj_start)
+      
+                                   
     def stream_start(self):
-        print("Start detekcji na strumieniu: \n")
+        
+        filmOrCam = 2      
+        print("\nStart detekcji na strumieniu: \n")
         if self.ui.detekcja_zajezdnia_checkBox.isChecked():
-            #  tel.zajezdnia = True
+            tel['peron'] = True
             print("Detekcja zajezdni aktywna")
+            
         if self.ui.detekcja_perony_checkBox.isChecked():
-            # tel.peron = True
+            tel['zajezdnia'] = True
             print("Detekcja peronow aktywna")
+            
         if self.ui.detekcja_przeszkody_checkBox.isChecked():
-            # tel.tory = True
+            tel['przeszkoda'] = True
             print("Detekcja przeszkod aktywna")
+            
         if self.ui.detekcja_twarz_checkBox.isChecked():
-            # tel.twarz = True
+            tel['twarz'] = True
             print("Detekcja twarzy aktywna")
+            
         if self.ui.detekcja_reka_checkBox.isChecked():
-            #  tel.reka = True
+            tel['reka'] = True
             print("Detekcja ruchu reka aktywna")
+            
         if self.ui.detekcja_ruch_checkBox.isChecked():
-            print("Detekcja ruchu pociagu aktywna")                                       
-                
-    def nagranie_start(self):
-        print("Start detekcji na nagraniu: \n")
-        if self.ui.detekcja_zajezdnia_checkBox.isChecked():
-            print("Detekcja zajezdni aktywna")
-            # tel.zajezdnia = True
-        if self.ui.detekcja_perony_checkBox.isChecked():
-            print("Detekcja peronow aktywnea")
-            #tel.peron = True
-        if self.ui.detekcja_przeszkody_checkBox.isChecked():
-            print("Detekcja przeszkod aktywna")
-            #tel.tory = True
-        if self.ui.detekcja_twarz_checkBox.isChecked():
-            print("Detekcja twarzy aktywna")
-            #tel.twarz = True
-        if self.ui.detekcja_reka_checkBox.isChecked():
-            print("Detekcja ruchu reka aktywna")
-            #tel.reka = True
-        if self.ui.detekcja_ruch_checkBox.isChecked():  
+            tel['ruch'] = True
             print("Detekcja ruchu pociagu aktywna")   
             
-   # def kalibruj(self):
+        if self.ui.detekcja_czerwony_pociag_checkBox.isChecked():
+            tel['czerwony'] = True
+            print("Detekcja czerwonego pociągu aktywna")
+                              
+    def nagranie_start(self):
         
+        filmOrCam = 1      
+        print("\nStart detekcji na nagraniu: \n")
+        if self.ui.detekcja_zajezdnia_checkBox.isChecked():
+            tel['peron'] = True
+            print("Detekcja zajezdni aktywna")
             
+        if self.ui.detekcja_perony_checkBox.isChecked():
+            tel['zajezdnia'] = True
+            print("Detekcja peronow aktywna")
+
+        if self.ui.detekcja_przeszkody_checkBox.isChecked():
+            tel['przeszkoda'] = True
+            print("Detekcja przeszkod aktywna")
+
+        if self.ui.detekcja_twarz_checkBox.isChecked():
+            tel['twarz'] = True
+            print("Detekcja twarzy aktywna")
+
+        if self.ui.detekcja_reka_checkBox.isChecked():
+            tel['reka'] = True
+            print("Detekcja ruchu reka aktywna")
+
+        if self.ui.detekcja_ruch_checkBox.isChecked(): 
+            tel['ruch'] = True
+            print("Detekcja ruchu pociagu aktywna")   
+            
+        if self.ui.detekcja_czerwony_pociag_checkBox.isChecked():
+            tel['czerwony'] = True
+            print("Detekcja czerwonego pociągu aktywna")  
+   # def kalibruj(self
         
 if __name__ == '__main__':
     qApp = QApplication(sys.argv)
