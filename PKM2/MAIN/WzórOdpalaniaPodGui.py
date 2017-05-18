cef34e4ecead1c7543a4f79ac918bd76bcd66aab
da_wid <da_wid@onet.pl>
2017-05-18 23:19:36 +0200

    próba przywrócenia tego nad czym dzisiaj pracowaliśmy

--- PKM2/MAIN/WzórOdpalaniaPodGui.py
+++ PKM2/MAIN/WzórOdpalaniaPodGui.py
@@ -1,5 +1,5 @@
 import os
-tel = {'peron':True,'zajezdnia':False,'reka':False,'tory':False,"czerwony":False,'twarz':False}
+tel = {'peron':True,'zajezdnia':False,'reka':False,'przeszkody':True,"czerwony":False,'twarz':False}
 filmOrCam=1 #1== dla filmu ,obojetnie jaka inna liczba dla kamery
 os.system("python skryptRozdzielajacy.py "+str(filmOrCam)+ " czysty.avi "+str(tel))
 
